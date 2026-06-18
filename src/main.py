488e82fa1cea7b17adf2ca98c67b1d435041af4b
Leonardo Lima <limapsleo@gmail.com>
2026-06-18 20:59:09 -0300

    feat: implement logging infrastructure

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from utils.logger import get_logger
+
+logger = get_logger(__name__)
+
+logger.info("Teste de log INFO")
+logger.warning("Teste WARNING")
+logger.error("Teste ERROR")
---
+++ src/utils/logger.py
@@ -0,0 +1,36 @@
+import logging
+from logging.handlers import RotatingFileHandler
+from config import LOG_DIR
+
+LOG_DIR.mkdir(parents=True, exist_ok=True)
+
+
+def get_logger(nome: str) -> logging.Logger:
+    logger = logging.getLogger(nome)
+    logger.setLevel(logging.INFO)
+    
+    if not logger.handlers:
+        
+        formatter = logging.Formatter(
+            "%(asctime)s | %(levelname)s | %(name)s | %(message)s"
+        )
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(formatter)
+        
+        file_handler = RotatingFileHandler(
+            LOG_DIR / "pipeline.log",
+            maxBytes=1_500_000,
+            backupCount=8,
+            encoding="utf-8"
+        )
+        file_handler.setFormatter(formatter)
+        
+        console_handler.setLevel(logging.INFO)
+        file_handler.setLevel(logging.INFO)
+        
+        logger.addHandler(console_handler)
+        logger.addHandler(file_handler)
+        logger.propagate=False
+    
+    return logger
+
